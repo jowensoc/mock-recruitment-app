94ac52338f6bf98950fe9343b17217d24e4fca2b
John Owens <john.owens@opencastsoftware.com>
2025-09-04 07:57:57 +0100

    added new url domain part

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,7 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, route, prefix } from "@react-router/dev/routes";
 
 export default [
-    index("routes/search.tsx")
+    ...prefix("mock-recruitment-app", [
+        index("routes/search.tsx")
+    ])
 ] satisfies RouteConfig;
